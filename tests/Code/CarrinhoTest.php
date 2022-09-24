63af503c1d6e9d17d85b2b19b831b78fe99578ac
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2022-09-24 13:00:59 -0300

    aula 12

--- tests/Code/CarrinhoTest.php
+++ tests/Code/CarrinhoTest.php
@@ -17,7 +17,7 @@ class CarrinhoTest extends TestCase
         $this->assertTrue($classe);
     }
 
-    public function testAdicaoDeProdutosNoCarrin()
+    public function testAdicaoDeProdutosNoCarrinho()
     {
         $produto = new Produto();
         $produto->setName('Produto 1');
@@ -36,5 +36,40 @@ class CarrinhoTest extends TestCase
         $this->assertIsArray($carrinho->getProdutos());
         $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[0]);
         $this->assertInstanceOf('\\Code\\Produto', $carrinho->getProdutos()[1]);
+    }
+
+    public function testSeValoresDeProdutosNoCarrinhoEstaoCorretosConformePassado()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+        $produto->setPrice('19.99');
+        $produto->setSlug('produto-1');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto);
+
+        $this->assertEquals('Produto 1', $carrinho->getProdutos()[0]->getName());
+        $this->assertEquals(19.99, $carrinho->getProdutos()[0]->getPrice());
+        $this->assertEquals('produto-1', $carrinho->getProdutos()[0]->getSlug());
+    }
+
+    public function testSeTotalDeProdutosEValorDaCompraEstaoCorretos()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+        $produto->setPrice('19.99');
+        $produto->setSlug('produto-1');
+
+        $produto2 = new Produto();
+        $produto2->setName('Produto 2');
+        $produto2->setPrice('19.99');
+        $produto2->setSlug('produto-2');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto);
+        $carrinho->addProduto($produto2);
+
+        $this->assertEquals(2,$carrinho->getTotalProdutos());
+        $this->assertEquals(39.98,$carrinho->getTotalCompra());
     }
 }
