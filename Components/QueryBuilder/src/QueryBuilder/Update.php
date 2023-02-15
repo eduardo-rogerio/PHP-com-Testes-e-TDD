d3516e8243b4a4c20e6ad5ba7cd61a8f899aa682
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-02-15 16:04:31 -0300

    aula 38

--- Components/QueryBuilder/src/QueryBuilder/Update.php
+++ Components/QueryBuilder/src/QueryBuilder/Update.php
@@ -6,15 +6,23 @@ class Update
 {
     public function __construct(string $table, array $fields = [], array $conditions = [], $logicOperator = ' AND ')
     {
-        $this->sql = 'UPDATE ' . $table . ' SET ' . implode(', :', $fields) . ' WHERE ';
+        $this->sql = 'UPDATE ' . $table . ' SET ';
+
+        $set = '';
+
+        foreach ($fields as $f) {
+            $set .= $set ? ', ' . $f . ' = :' . $f
+                : $f . ' = :' . $f;
+        }
 
         $where = '';
 
         foreach ($conditions as $key => $c) {
-            $where .= $where ? $logicOperator . $key . ' = ' . $c : $key . ' = ' . $c;
+            $where .= $where ? $logicOperator . $key . ' = ' . $c
+                : ' WHERE ' . $key . ' = ' . $c;
         }
 
-        $this->sql .= $where;
+        $this->sql .= $set . $where;
     }
 
     public function getSql()
