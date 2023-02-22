0e89e6ec7c503488b19f7e1696e2ed4454f1e327
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2023-02-22 13:01:06 -0300

    aula 45

--- Components/QueryBuilder/src/QueryBuilder/Executor.php
+++ Components/QueryBuilder/src/QueryBuilder/Executor.php
@@ -7,10 +7,30 @@ class Executor
     private $connection;
 
     private $query;
+    private array $params = [];
 
     public function __construct(\PDO $connection, $query = null)
     {
         $this->connection = $connection;
         $this->query = $query;
+    }
+
+    public function setQuery($query)
+    {
+        $this->query = $query;
+    }
+
+    public function setParam($bind, $value)
+    {
+        $this->params[] = ['bind' => $bind, 'value' => $value];
+
+        return $this;
+    }
+
+    public function execute()
+    {
+        $proccess = $this->connection->prepare($this->query->getSql());
+
+        return 1;
     }
 }
--- Components/QueryBuilder/tests/ExecutorTest.php
+++ Components/QueryBuilder/tests/ExecutorTest.php
@@ -2,6 +2,7 @@
 
 namespace Code\QueryBuilder;
 
+use Code\QueryBuilder\Query\Insert;
 use PHPUnit\Framework\TestCase;
 
 class ExecutorTest extends TestCase
@@ -12,14 +13,14 @@ class ExecutorTest extends TestCase
 
     public static function setUpBeforeClass(): void
     {
-        self::$conn = new \PDO('sqlite::memory');
+        self::$conn = new \PDO('sqlite::memory:');
         self::$conn->exec("
-            CREATE TABLE IF NOT EXISTS 'products' (
+            CREATE TABLE IF NOT EXISTS 'products'(
                 'id' INTEGER PRIMARY KEY,
                 'name' TEXT,  
                 'price' FLOAT,  
                 'created_at' TIMESTAMP,  
-                'updated_at' TIMESTAMP,  
+                'updated_at' TIMESTAMP  
             );
         ");
 
@@ -29,5 +30,19 @@ class ExecutorTest extends TestCase
     public static function tearDownAfterClass(): void
     {
         self::$conn->exec('DROP TABLE products');
+    }
+
+    public function testInsertANewProductInADatabase()
+    {
+        $query = new Insert('products', ['name', 'price', 'created_at', 'updated_at']);
+
+        self::$executor->setQuery($query);
+
+        self::$executor->setParam(':name', 'Product 1')
+            ->setParam(':price', 19.99)
+            ->setParam(':created_at', date('Y-m-d H:i:s'))
+            ->setParam(':update_at', date('Y-m-d H:i:s'));
+
+        $this->assertEquals(1, self::$executor->execute());
     }
 }
